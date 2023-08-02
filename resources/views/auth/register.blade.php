21b851577182c438f3c6d3d13948598dff01ebdf
Sidibé Arouna <sidibearouna71@gmail.com>
2023-08-02 15:24:03 +0000

    changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,8 +5,8 @@
 
         <input type="hidden" name="_token" value="{{ csrf_token() }}" />
         <img class="mb-4" src="{!! url('images/bootstrap-logo.svg') !!}" alt="" width="72" height="57">
-        
-        <h1 class="h3 mb-3 fw-normal">Register</h1>
+
+                <h1 class="h3 mb-3 fw-normal">Register</h1>
 
         <div class="form-group form-floating mb-3">
             <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="name@example.com" required="required" autofocus>
@@ -23,8 +23,8 @@
                 <span class="text-danger text-left">{{ $errors->first('username') }}</span>
             @endif
         </div>
-        
-        <div class="form-group form-floating mb-3">
+
+        <div class="form        -group form-floating mb-3">
             <input type="password" class="form-control" name="password" value="{{ old('password') }}" placeholder="Password" required="required">
             <label for="floatingPassword">Password</label>
             @if ($errors->has('password'))
@@ -41,7 +41,7 @@
         </div>
 
         <button class="w-100 btn btn-lg btn-primary" type="submit">Register</button>
-        
+
         @include('auth.partials.copy')
-    </form>
+            </form>
 @endsection
